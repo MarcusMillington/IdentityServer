4d86d4ab8d80da2e7af51313850270ccc6744840
Marcus <marcus@bluestreamacademy.com>
2018-07-13 17:02:06 +0100

    First Commit

---
+++ IdentityServer/Areas/Identity/Services/EmailSender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Identity.UI.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
